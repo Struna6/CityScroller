b652832eb718d10f54be62db3fd7a6269d486381
Struna6 <struniawski.karol@gmail.com>
2019-03-23 10:25:49 +0100

    right scrolling ready

--- CityScroller/ViewController.swift
+++ CityScroller/ViewController.swift
@@ -8,13 +8,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ let height = CGFloat.random(min: 350, max: maxBuildingHeight)
+ let width = CGFloat.random(min: 120, max: 200)
+ class var maxBuildingHeight: CGFloat {
+ return UIScreen.main.bounds.height + 50.0
+ }
+ */
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    var buildings = [BuildingView]()
+    var buildingsLeftCorner = CGFloat()
+    var buildingsRightCorner = CGFloat()
+    var moon = UIView()
+    var setup = true
+
+    func loadBuildings(){
+        var moveBy = 400.0
+        while moveBy <= 400 + Double(self.view.bounds.maxX){
+            let building = BuildingView.randomBuilding()
+            building.center = CGPoint(x: CGFloat(moveBy) + (building.frame.width/2), y: scrollView.bounds.height - (building.frame.height/2) + 50)
+            moveBy = moveBy + Double(building.frame.width)
+            buildingsRightCorner = building.frame.maxX
+            buildings.append(building)
+            scrollView.addSubview(building)
+        }
+    }
+
+    func spawnMoon(){
+        let moonRect = CGRect(origin: .zero, size: CGSize(width: 80, height: 80))
+        let moon = UIView(frame: moonRect)
+        moon.center = CGPoint(x: 50, y: 100)
+        moon.backgroundColor = .yellow
+        moon.layer.cornerRadius = moon.frame.width / 2
+        self.moon = moon
+        scrollView.addSubview(moon)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: self.view.bounds.width + 800, height: UIScreen.main.bounds.height + 50.0)
+        scrollView.backgroundColor = .darkGray
+
+        scrollView.contentOffset.y = 50.0
+        scrollView.contentOffset.x = 400.0
+        buildingsLeftCorner = 400.0
+        scrollView.isDirectionalLockEnabled = true
     }
 
+    override func viewWillLayoutSubviews() {
 
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setup = true
+        spawnMoon()
+        loadBuildings()
+        setup = false
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        moon.frame.origin.x = scrollView.contentOffset.x + 10.0
+        print(buildings.count)
+
+        if scrollView.contentOffset.x + self.view.bounds.width >= buildingsRightCorner && !setup{
+            spawnBuildingRight()
+        }
+
+        if scrollView.contentOffset.x <= buildingsRightCorner && !setup{
+            spawnBuildingLeft()
+        }
+
+        if let firstBuildingWidth = buildings.first?.frame.width{
+            if scrollView.contentOffset.x >= buildingsLeftCorner + firstBuildingWidth{
+                let width = buildings.first?.frame.width
+                buildings.first?.removeFromSuperview()
+                buildings.removeFirst()
+                buildings.forEach { (bld) in
+                    bld.center.x -= width!
+                }
+                buildingsLeftCorner = (buildings.first?.frame.minX)!
+                buildingsRightCorner = (buildings.last?.frame.maxX)!
+                scrollView.contentOffset.x = buildingsLeftCorner
+            }
+        }
+    }
+
+    func spawnBuildingRight(){
+        let building = BuildingView.randomBuilding()
+        building.center = CGPoint(x: buildingsRightCorner + (building.frame.width/2), y: scrollView.bounds.height - (building.frame.height/2) + 50)
+        buildingsRightCorner = building.frame.maxX
+        buildings.append(building)
+        scrollView.addSubview(building)
+    }
+
+    func spawnBuildingLeft(){
+        
+    }
 }
 
